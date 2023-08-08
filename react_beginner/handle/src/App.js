79480644bb63faca731bc825341ee1c4607897b3
choiyun9yu <110392046+choiyun9yu@users.noreply.github.com>
2023-08-08 17:06:24 +0900

    main

--- react_beginner/handle/src/App.js
+++ react_beginner/handle/src/App.js
@@ -4,6 +4,9 @@ import Button from "./Button";
 import { useState } from "react";
 import { compareHand, generateRandomHand } from './utils';
 
+
+const INITIAL_VALUE = 'rock';
+
 function getResult(me, other) {
   const comparison = compareHand(me, other);
   if (comparison > 0) return '승리';
@@ -12,7 +15,7 @@ function getResult(me, other) {
 }
 
 
-function App() { 
+function App() {
   // const handleClick = (value) => console.log(value);
 
   // return (
@@ -27,28 +30,62 @@ function App() {
   //   </div>
   // );
 
-  const [hand, setValue] = useState('rock');
-  const [otherHand, setOtherValue] = useState('scissor')
+  // 패
+  const [hand, setHand] = useState(INITIAL_VALUE);
+  const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
+  const [gameHistory, setGameHistory] = useState([]);
+  // 점수
+  const [score, setScore] = useState(0);
+  const [otherScore, setOtherScore] = useState(0);
+  const [bet, setBet] = useState(1);
 
   const handleButtonClick = (nextHand) => {
-    setValue(nextHand);
-    setOtherValue(generateRandomHand());
+    const nextOtherHand = generateRandomHand();
+    const nextHistoryItem = getResult(nextHand, nextOtherHand);
+    const comparison = compareHand(nextHand, nextOtherHand);
+    setHand(nextHand);
+    setOtherHand(nextOtherHand);
+    setGameHistory([...gameHistory, nextHistoryItem]);
+    if (comparison > 0) setScore(score + bet);
+    if (comparison < 0) setOtherScore(otherScore + bet);
   };
 
   const handleClearClick = () => {
-    setValue('rock');
-    setOtherValue('scissor');
+    setHand(INITIAL_VALUE);
+    setOtherHand(INITIAL_VALUE);
+    setGameHistory([]);
+    setScore(0);
+    setOtherScore(0);
+    setBet(1);
+  };
+
+  const handleBetChange = (e) => {
+    // input의 value 속성을 참조하려면 e.target.value로 가져올 수 있다.
+    // 그러나 이 값은 문자열이기 때문에 Number 생성자를 써서 숫자형으로 바꿔줘야 한다.
+    let num = Number(e.target.value);
+    if (num > 9) num %= 10; // 1과 9 사이의 숫자로 만들어 줌
+    if (num < 1) num = 1;
+    
+    num = Math.floor(num);
+    setBet(num);
   };
 
   return (
     <div>
       <Button onClick={handleClearClick}>처음부터</Button>
-      <p>{getResult(hand, otherHand)}</p>
+      <div>
+        {score} : {otherScore}
+      </div>
       <div>
         <HandIcon value={hand} />
         VS
         <HandIcon value={otherHand} />
       </div>
+      {/* 배점을 정하는 input tag */}
+      <div>
+        <input type="number" value={bet} min={1} max={9} onChange={handleBetChange}></input>
+      </div>
+      <p>승부 기록: {gameHistory.join(', ')}</p>
       <div>
         <HandButton value="rock" onClick={handleButtonClick} />
         <HandButton value="scissor" onClick={handleButtonClick} />
