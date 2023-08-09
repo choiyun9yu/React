d1b1ccf7b36390e0591abd2a316d3a1f21fb64ce
choiyun9yu <110392046+choiyun9yu@users.noreply.github.com>
2023-08-09 09:16:03 +0900

    main

--- ex3_movie/src/components/ReviewList.js
+++ ex3_movie/src/components/ReviewList.js
@@ -1,6 +1,40 @@
+import "./ReviewList.css";
+
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
+function ReviewListItem({ item }) {
+  return (
+    <div>
+      <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
+      <div>
+        <h1>{item.title}</h1>
+        <p>{item.rating}</p>
+        {/*  생성한 날짜를 나타내는 createdAt은 숫자형 */}
+        <p>{formatDate(item.createdAt)}</p>
+        <p>{item.content}</p>
+      </div>
+    </div>
+  );
+}
+
 function ReviewList({ items }) {
   console.log(items);
-  return <ul></ul>;
+  return (
+    <ul>
+      {/* // map 메소드는 콜백함수를 실행한 결과를 가지고 새로운 배열을 만들때 사용
+      // map 메소드 안에서 JSX를 리턴하면, JSX를 여러개 추가한 것 처럼 동작 */}
+      {items.map((item) => {
+        return (
+          <li>
+            <ReviewListItem item={item} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default ReviewList;
--- ex3_movie/src/index.js
+++ ex3_movie/src/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
+// 1. map으로 배열 렌더링하기
+// 많은 데이터를 보여줄 때 유용
+// map 메소드는 콜백함수를 실행한 결과를 가지고 새로운 배열을 만들때 사용
+// map 메소드 안에서 JSX를 리턴하면, JSX를 여러개 추가한 것 처럼 동작
+
+// 2. 데이터 가져오기
+
+// 3. 입력 폼 다루기
+
+// 4. 데이터 보내기
+
+// 5. 전역 데이터 다루기
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<App />);
