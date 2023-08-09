6a903096c34d6962e137db9d299132655b432891
choiyun9yu <110392046+choiyun9yu@users.noreply.github.com>
2023-08-09 09:00:17 +0900

    main

---
+++ ex3_movie/src/components/App.js
@@ -0,0 +1,14 @@
+// 프로젝트 최상위 컴포넌트
+
+import ReviewList from "./ReviewList";
+import items from "../mock.json";
+
+function App() {
+  return (
+    <div>
+      <ReviewList items={items} />
+    </div>
+  );
+}
+
+export default App;
