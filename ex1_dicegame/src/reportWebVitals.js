bfafd9eba211ad831f5406e17c0bf1190852425d
choiyun9yu <110392046+choiyun9yu@users.noreply.github.com>
2023-08-09 09:37:32 +0900

    main

--- ex1_dicegame/src/reportWebVitals.js
+++ ex1_dicegame/src/reportWebVitals.js
@@ -1,6 +1,7 @@
-const reportWebVitals = onPerfEntry => {
+//. 리액트 성능 측정을 위한 모듈
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
